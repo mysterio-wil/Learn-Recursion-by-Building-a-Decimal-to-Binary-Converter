208335f7ceacc9171ca8e7d3ca5d26ed3e2136cd
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-10 23:08:10 -0500

    Validación de entrada en función checkUserInput
    - Se agregó la validación en la función checkUserInput para asegurar que el usuario ingrese un número decimal positivo.
    - La validación comprueba si el campo de entrada está vacío (!numberInput.value), si el valor ingresado es un número (isNaN(parseInt(numberInput.value))), y si el número es mayor o igual a cero (parseInt(numberInput.value) < 0).
    Si alguna de estas condiciones no se cumple, se muestra una alerta con el mensaje "Please provide a decimal number greater than or equal to 0" y se detiene la ejecución.
    - Esta mejora evita que valores inválidos interrumpan el flujo del programa, brindando una mejor experiencia al usuario.

--- script.js
+++ script.js
@@ -7,10 +7,18 @@ const convertBtn = document.getElementById("convert-btn");
 // Selecciona el elemento donde se mostrará el resultado de la conversión a binario
 const result = document.getElementById("result");
 
-// Función para verificar la entrada del usuario e imprimir su valor en la consola
 const checkUserInput = () => {
-    console.log(numberInput.value);
-  };
+    // Verifica si el campo de entrada está vacío, si no es un número o si es negativo
+    if (
+        !numberInput.value || // Verifica si el campo está vacío
+        isNaN(parseInt(numberInput.value)) || // Verifica si el valor ingresado no es un número
+        parseInt(numberInput.value) < 0 // Verifica si el número es negativo
+    ) {
+        // Muestra una alerta indicando que se necesita un número decimal positivo
+        alert("Please provide a decimal number greater than or equal to 0");
+        return; // Sale de la función si la entrada no es válida
+    }
+};
 
 // Agrega un listener de evento al botón para ejecutar la función checkUserInput al hacer clic
 convertBtn.addEventListener("click", checkUserInput);
@@ -18,7 +26,6 @@ convertBtn.addEventListener("click", checkUserInput);
 // Agrega un listener de evento al campo de entrada para ejecutar checkUserInput al presionar Enter
 numberInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
-      checkUserInput();
+        checkUserInput();
     }
-  });
-  
+});
