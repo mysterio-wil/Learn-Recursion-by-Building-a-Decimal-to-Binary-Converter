8e8b52d1bda11bbae6b0c9d51f219e7e1d2a9138
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-10 20:33:22 -0500

    Agregar función `checkUserInput` para verificar e imprimir el valor de entrada del usuario en la consola

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const convertBtn = document.getElementById("convert-btn");
 
 // Selecciona el elemento donde se mostrará el resultado de la conversión a binario
 const result = document.getElementById("result");
+
+// Función para verificar la entrada del usuario e imprimir su valor en la consola
+const checkUserInput = () => {
+    console.log(numberInput.value);
+  };
