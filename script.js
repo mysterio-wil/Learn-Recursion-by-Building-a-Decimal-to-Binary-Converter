8fab556a37849def8d91fbfca3d910fa33f8f0be
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-11-10 20:26:11 -0500

    Inicializar elementos de la interfaz: número de entrada, botón de conversión y área de resultado

---
+++ script.js
@@ -0,0 +1,8 @@
+// Selecciona el campo de entrada donde los usuarios ingresarán un número decimal
+const numberInput = document.getElementById("number-input");
+
+// Selecciona el botón que activará la conversión a binario
+const convertBtn = document.getElementById("convert-btn");
+
+// Selecciona el elemento donde se mostrará el resultado de la conversión a binario
+const result = document.getElementById("result");
